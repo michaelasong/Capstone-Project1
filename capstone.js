d60a041f3d5db4394e9ab69d401b4958cfd939a2
michaelasong <michaelasong237@gmail.com>
2022-08-06 17:12:38 +0100

    Capstone Project Week 5

---
+++ capstone.js
@@ -0,0 +1,74 @@
+const speakers = [
+  {
+    name: 'Miles Obi',
+    post: 'Principal',
+    image: '/Images/personel 1.png',
+    summary:
+      'The Principal\'s main focus should be to develop and maintain effective educational programs within his/her school and to promote the improvement of teaching and learning with his/her school.',
+  },
+  {
+    name: 'Ojong Joel',
+    post: 'Vice Principal',
+    image: '/Images/personel 1.png',
+    summary: 'looooo',
+  },
+  {
+    name: 'Tangdou Helen',
+    post: 'Discipline Master',
+    image: '/Images/personel2.png',
+    summary: 'Niiiiii',
+  },
+  {
+    name: 'Mario Ajeba',
+    post: 'Vice Principal',
+    image: '/Images/personel 1.png',
+    summary: '....',
+  },
+  {
+    name: 'Atangana Marie therese.F',
+    post: 'Vice Principal',
+    image: '/Images/personel4.png',
+    summary: ',,,,,',
+  },
+  {
+    name: 'Tagne Kevin J.M',
+    post: 'Vice Principal',
+    image: '/Images/personel 3.png',
+    summary: 'hi hi hi',
+  },
+];
+
+function featureSpeakers() {
+  const sectn3 = document.querySelector('.Speaker');
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < speakers.length; i++) {
+    const li = document.createElement('li');
+
+    const div1 = document.createElement('div');
+    div1.className = 'be4';
+    const div2 = document.createElement('div');
+
+    li.appendChild(div1);
+    li.appendChild(div2);
+
+    const img = document.createElement('img');
+    div1.appendChild(img);
+
+    const h4 = document.createElement('h4');
+    const h5 = document.createElement('h5');
+    const p = document.createElement('p');
+
+    div2.appendChild(h4);
+    div2.appendChild(h5);
+    div2.appendChild(p);
+
+    sectn3.append(li);
+    h4.innerHTML = speakers[i].name;
+    h5.innerHTML = speakers[i].post;
+    p.innerHTML = speakers[i].summary;
+    img.src = speakers[i].image;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (e) => featureSpeakers(e));
